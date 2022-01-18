7fb1678ff4dbd89e253d127dce17cb6cb87fa787
Sherin Varughese <sherin.a.varughese@gmail.com>
2022-01-18 17:25:01 -0500

    Create search schema to add tables (#16)
    * add search schema
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * no updates to syncHandler
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * revert updates to syncHandler
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * no lint error check
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * add sonar properties
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- pkg/database/connection_test.go
+++ pkg/database/connection_test.go
@@ -11,11 +11,11 @@ import (
 func Test_initializeTables(t *testing.T) {
 	// Prepare a mock DAO instance
 	dao, mockPool := buildMockDAO(t)
-
-	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("DROP TABLE resources")).Return(nil, nil)
-	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("DROP TABLE edges")).Return(nil, nil)
-	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("CREATE TABLE IF NOT EXISTS edges (sourceId TEXT, sourceKind TEXT,destId TEXT,destKind TEXT,edgeType TEXT,cluster TEXT)")).Return(nil, nil)
-	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("CREATE TABLE IF NOT EXISTS resources (uid TEXT PRIMARY KEY, cluster TEXT, data JSONB)")).Return(nil, nil)
+	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("CREATE SCHEMA IF NOT EXISTS search")).Return(nil, nil)
+	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("DROP TABLE search.resources")).Return(nil, nil)
+	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("DROP TABLE search.edges")).Return(nil, nil)
+	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("CREATE TABLE IF NOT EXISTS search.edges (sourceId TEXT, sourceKind TEXT,destId TEXT,destKind TEXT,edgeType TEXT,cluster TEXT)")).Return(nil, nil)
+	mockPool.EXPECT().Exec(gomock.Any(), gomock.Eq("CREATE TABLE IF NOT EXISTS search.resources (uid TEXT PRIMARY KEY, cluster TEXT, data JSONB)")).Return(nil, nil)
 
 	// Execute function test.
 	dao.InitializeTables()
