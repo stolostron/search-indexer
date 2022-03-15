de28a9df6e78f31771e6ed9a1c55e22f417ecda1
Jorge Padilla <jpadilla@redhat.com>
2022-03-15 12:39:37 -0400

    cleanup
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/clustersync/clusterSync.go
+++ pkg/clustersync/clusterSync.go
@@ -5,8 +5,19 @@ package clustersync
 import (
 	"context"
 
+	"github.com/stolostron/search-indexer/pkg/config"
 	klog "k8s.io/klog/v2"
 )
+
+func ElectLeaderAndStart(ctx context.Context) {
+	client := config.Cfg.KubeClient
+	lockName := "search-indexer.open-cluster-management.io"
+	podName := config.Cfg.PodName
+	podNamespace := config.Cfg.PodNamespace
+
+	lock := getNewLock(client, lockName, podName, podNamespace)
+	runLeaderElection(ctx, lock)
+}
 
 func syncClusters(ctx context.Context) {
 	klog.Info("TODO: Start Sync clusters here.")
