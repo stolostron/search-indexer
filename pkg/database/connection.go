a34cc06046e517d619c170fb67c017d25f23aa5f
Sherin Varughese <shvarugh@redhat.com>
2023-01-24 14:16:32 -0500

    add default false to overcome ERROR: null value in column deleted
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- pkg/database/connection.go
+++ pkg/database/connection.go
@@ -105,7 +105,7 @@ func (dao *DAO) InitializeTables(ctx context.Context) {
 		"CREATE TABLE IF NOT EXISTS search.resources_hist (uid TEXT, cluster TEXT, updated TIMESTAMP, deleted BOOLEAN, PRIMARY KEY(uid, updated))")
 	checkError(err, "Error creating table search.resources_hist.")
 	_, err = dao.pool.Exec(ctx,
-		"CREATE TABLE IF NOT EXISTS search.edges_hist (sourceId TEXT, sourceKind TEXT,destId TEXT,destKind TEXT,edgeType TEXT,cluster TEXT, updated TIMESTAMP, deleted BOOLEAN, PRIMARY KEY(sourceId, destId, edgeType, deleted))")
+		"CREATE TABLE IF NOT EXISTS search.edges_hist (sourceId TEXT, sourceKind TEXT,destId TEXT,destKind TEXT,edgeType TEXT,cluster TEXT, updated TIMESTAMP, deleted BOOLEAN DEFAULT FALSE, PRIMARY KEY(sourceId, destId, edgeType, deleted))")
 	checkError(err, "Error creating table search.edges_hist.")
 
 	// Jsonb indexing data keys:
