8745be592695cd7ddea80726d1f92a0dd0e3836a
Jorge Padilla <jpadilla@redhat.com>
2023-01-31 17:12:26 -0500

    Debug
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/server/validateRequestMiddleware.go
+++ pkg/server/validateRequestMiddleware.go
@@ -13,15 +13,20 @@ import (
 	"github.com/stolostron/search-indexer/pkg/config"
 )
 
+var pendingRequests = map[string]time.Time{}
+var pendingLock = sync.RWMutex{}
+
 // Checks if we are able to accept the incoming request.
 func ValidateRequestMiddleware(next http.Handler) http.Handler {
 
-	pendingRequests := map[string]time.Time{}
-	pendingLock := sync.RWMutex{}
+	// pendingRequests := map[string]time.Time{}
+	// pendingLock := sync.RWMutex{}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		params := mux.Vars(r)
 		clusterName := params["id"]
+
+		klog.Info("Inside Validate Middleware. Pending requests:", len(pendingRequests))
 
 		if t, exists := pendingRequests[clusterName]; exists {
 			klog.Warningf("Rejecting request from %s because there's a previous request processing. Duration: %s",
