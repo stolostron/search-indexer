903d2ad65003d3c0c65e98b835973eabdb665552
Jorge Padilla <jpadilla@redhat.com>
2023-02-03 09:06:14 -0500

    Limit concurrent requests to prevent OOM (ACM-3065) (#78)
    * Limit concurrent requests to prevent OOM
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Add middleware
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * ACM-3065 Add test
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Added slow log and changed verbosity.
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Add slowLog
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update log msg
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update log msg
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * SlowLog default to 500ms
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Debug
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Reduce log
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Add tests for limiter handler
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Add success test
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Rename MaxConns
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update comment
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update comment
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Typo in log
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Rename DB_MAX_CONNS
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Rename requestsProcessing map
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Review comments
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Renamed requestTracker and lock
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Fix lock
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    ---------
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

---
+++ pkg/server/requestLimiter_test.go
@@ -0,0 +1,81 @@
+// Copyright Contributors to the Open Cluster Management project
+package server
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Verify that request is accepted with 3 pending requests.
+func Test_requestLimiterMiddleware(t *testing.T) {
+	// Mock 3 pending requests.
+	requestTracker = map[string]time.Time {"A": time.Now(), "B": time.Now(), "C": time.Now()}
+
+	requestLimiterHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	req := httptest.NewRequest("POST", "https://localhost:3010/aggregator/clusters/cluster1/sync", nil)
+	res := httptest.NewRecorder()
+
+	requestLimiterHandler(res, req)
+	middleware := requestLimiterMiddleware(requestLimiterHandler)
+
+	middleware.ServeHTTP(res, req)
+
+	// Validate response code and messsage.
+	assert.Equal(t, http.StatusOK, res.Code)
+}
+
+// Verify that request is rejected when there's a pending request form the same cluster.
+func Test_requestLimiterMiddleware_existingRequest(t *testing.T) {
+	// Mock a pending request from cluster.
+	// NOTE: Omitting the cluster name to keep the test simple, otherwise we would need to mock
+	// the mux router so the handler can read the cluster {id} from the route.
+	requestTracker = map[string]time.Time {"": time.Now()}
+
+	// Mock Request and Response.
+	requestLimiterHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	req := httptest.NewRequest("POST", "https://localhost:3010/aggregator/clusters/cluster1/sync", nil)
+	res := httptest.NewRecorder()
+	requestLimiterHandler(res, req)
+	middleware := requestLimiterMiddleware(requestLimiterHandler)
+
+	// Execute middleware.
+	middleware.ServeHTTP(res, req)
+
+	// Validate response code.
+	assert.Equal(t, http.StatusTooManyRequests, res.Code)
+
+	bodyBytes, _ := io.ReadAll(res.Body)
+	assert.Equal(t, "A previous request from this cluster is processing, retry later.\n", string(bodyBytes))
+
+}
+
+// Verify that request is rejected when there's 50 or more pending requests.
+func Test_requestLimiterMiddleware_with50PendingRequests(t *testing.T) {
+	// Mock 50 pending requests.
+	requestTracker = map[string]time.Time {}
+	for i:=0; i<50; i++{
+		requestTracker["cluster" + strconv.Itoa(i)] = time.Now()
+	}
+
+	requestLimiterHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	req := httptest.NewRequest("POST", "https://localhost:3010/aggregator/clusters/cluster99/sync", nil)
+	res := httptest.NewRecorder()
+
+	requestLimiterHandler(res, req)
+	middleware := requestLimiterMiddleware(requestLimiterHandler)
+
+	middleware.ServeHTTP(res, req)
+
+	// Validate response code and messsage.
+	assert.Equal(t, http.StatusTooManyRequests, res.Code)
+
+	bodyBytes, _ := io.ReadAll(res.Body)
+	assert.Equal(t, "Indexer has too many pending requests, retry later.\n", string(bodyBytes))
+
+}
