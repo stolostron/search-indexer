3b51e21e8133adba7c7e40a2ba16e4830853bbd1
Jorge Padilla <jpadilla@redhat.com>
2023-07-12 16:28:29 -0400

    Improve locust scale tests (#103)
    * Saving progress
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Update resync logic and locust
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Remove locust caches
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Remove locust caches
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Remove changes to resync logic from this PR
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Cleanup locust changes
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Cleanup locust changes
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Finalize locust changes
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    * Remove changes on syncHandler from this PR
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>
    
    ---------
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- test/locust-clusters.py
+++ test/locust-clusters.py
@@ -14,43 +14,59 @@ class ClusterBehavior(TaskSet):
 #  2. Update state.
 
     def send_full_state_payload(self):
-        with open("sno-template.json", "r") as template_file:
+        print("%s - sending full state" % self.user.name)
+        with open("cluster-templates/{}".format(self.user.template), "r") as template_file:
             template_string = template_file.read().replace("<<CLUSTER_NAME>>", self.user.name)
         f = io.StringIO(template_string)
         j = json.load(f)        
         self.client.payload = j
         self.do_post()
-        print("%s - sent full state" % self.user.name)
 
     def send_update_payload(self):
-        f = open("cluster-update-template.json",)
+        print("%s - sending update" % self.user.name)
+        f = open("cluster-templates/add-resources.json",)
         j = json.load(f)        
         for resource in j["addResources"]: 
             resource["uid"] = "{}/{}".format(self.user.name, str(uuid.uuid4()) )
             resource["properties"]["name"] = "gen-name-{}".format(str(uuid.uuid4()) )
         self.client.payload = j
         self.do_post()
-        print("%s - sent update" % self.user.name)
 
     def do_post(self):
-        self.client.post("/aggregator/clusters/{}/sync".format(self.user.name), json=self.client.payload, verify=False)
+        resp = self.client.post("/aggregator/clusters/{}/sync".format(self.user.name), json=self.client.payload, verify=False)
+        print("[%s] response code: %s" % (self.user.name, resp.status_code))
+        if resp.status_code != 200: # The first request is receiving 0 instead of 429.
+            self.user.retries = self.user.retries + 1
+            print("\t> %s Indexer was busy. Waiting %d seconds and retrying." % (self.user.name, self.user.retries * 2))
+            time.sleep(self.user.retries * 2)
+            self.do_post()
+        else:
+            print("%s - completed do_post() with %s retries." % (self.user.name, self.user.retries))
+            self.user.retries = 0
+
 
     def on_start(self):
         self.send_full_state_payload()
-        time.sleep(120)
+        time.sleep(60)
 
-    @task
+    @task(10)
     def send_update(self):
         self.send_update_payload()
+
+    @task(1)
+    def send_resync(self):
+        self.send_full_state_payload()
        
 # A Cluster is equivalent to a User.
 class Cluster(HttpUser):
     name = ""
     tasks = [ClusterBehavior]
-    wait_time = between(30, 300)
+    wait_time = between(5, 300)
+    retries = 0
+    template = "sno-5k.json" # sno-100k.json, sno-150k.json
 
     def on_start(self):
         global clusterCount
-        self.name = "cluster{}".format(clusterCount)
+        self.name = "locust-{}".format(clusterCount)
         clusterCount = clusterCount + 1
         print("Starting cluster [%s]" % self.name)
