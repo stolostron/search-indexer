56db2b5537d76872eaad7cdf2ad1f86d922b6710
Jorge Padilla <jpadilla@redhat.com>
2023-07-12 12:37:02 -0400

    Cleanup locust changes
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- test/locust-clusters.py
+++ test/locust-clusters.py
@@ -14,34 +14,34 @@ class ClusterBehavior(TaskSet):
 #  2. Update state.
 
     def send_full_state_payload(self):
-        with open("cluster-templates/sno-100k.json", "r") as template_file:
+        print("%s - sending full state" % self.user.name)
+        with open("cluster-templates/sno-5k.json", "r") as template_file:
             template_string = template_file.read().replace("<<CLUSTER_NAME>>", self.user.name)
         f = io.StringIO(template_string)
         j = json.load(f)        
         self.client.payload = j
         self.do_post()
-        print("%s - sent full state" % self.user.name)
 
     def send_update_payload(self):
-        f = open("cluster-update-template.json",)
+        print("%s - sending update" % self.user.name)
+        f = open("cluster-templates/add-resources.json",)
         j = json.load(f)        
         for resource in j["addResources"]: 
             resource["uid"] = "{}/{}".format(self.user.name, str(uuid.uuid4()) )
             resource["properties"]["name"] = "gen-name-{}".format(str(uuid.uuid4()) )
         self.client.payload = j
         self.do_post()
-        print("%s - sent update" % self.user.name)
 
     def do_post(self):
         resp = self.client.post("/aggregator/clusters/{}/sync".format(self.user.name), json=self.client.payload, verify=False)
         print("[%s] response code: %s" % (self.user.name, resp.status_code))
-        if resp.status_code != 200: # 429
+        if resp.status_code != 200: # The first request is receiving 0 instead of 429.
             self.user.retries = self.user.retries + 1
             print("[%s] Indexer was busy. Waiting %d seconds and retrying." % (self.user.name, self.user.retries * 2))
             time.sleep(self.user.retries * 2)
             self.do_post()
         else:
-            print("[%s] Completed do_post() with %s retries." % (self.user.name, self.user.retries))
+            print("%s - completed do_post() with %s retries." % (self.user.name, self.user.retries))
             self.user.retries = 0
 
 
@@ -51,14 +51,17 @@ class ClusterBehavior(TaskSet):
 
     @task
     def send_update(self):
-        # self.send_update_payload()
+        self.send_update_payload()
+
+    @task
+    def send_resync(self):
         self.send_full_state_payload()
        
 # A Cluster is equivalent to a User.
 class Cluster(HttpUser):
     name = ""
     tasks = [ClusterBehavior]
-    wait_time = between(30, 60)
+    wait_time = between(30, 300)
     retries = 0
 
     def on_start(self):
