5b01ea7392dddc373589e0b9789e014b494220e2
Sherin Varughese <shvarugh@redhat.com>
2023-12-06 11:27:22 -0500

    ignore lint
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- pkg/database/goquHelper.go
+++ pkg/database/goquHelper.go
@@ -19,18 +19,19 @@ func useGoqu(query string, params []interface{}) (q string, p []interface{}, er 
 	case "SELECT uid, data FROM search.resources WHERE cluster=$1 AND uid!='cluster__$1'":
 		q, p, er = dialect.From(resources).Prepared(true).
 			Select("uid", "data").Where(
-			goqu.C("cluster").Eq(params[0]),
+			goqu.C("cluster").Eq(params[0]),                           // nolint:staticcheck //lint:ignore G602
 			goqu.C("uid").Neq(fmt.Sprintf("cluster__%s", params[0]))). // Exclude the cluster pseudo-node.
 			ToSQL()
 
 	case "INSERT into search.resources values($1,$2,$3) ON CONFLICT (uid) DO NOTHING":
 		q, p, er = dialect.From(resources).Prepared(true).
-			Insert().Rows(goqu.Record{"uid": params[0], "cluster": params[1], "data": params[2]}).
+			Insert().
+			Rows(goqu.Record{"uid": params[0], "cluster": params[1], "data": params[2]}). // nolint:staticcheck //lint:ignore G602
 			OnConflict(goqu.DoNothing()).ToSQL()
 
 	case "UPDATE search.resources SET data=$2 WHERE uid=$1":
 		q, p, er = dialect.From(resources).Prepared(true).
-			Update().Set(goqu.Record{"data": params[1].(string)}).Where(goqu.C("uid").Eq(params[0])).ToSQL()
+			Update().Set(goqu.Record{"data": params[1].(string)}).Where(goqu.C("uid").Eq(params[0])).ToSQL() // nolint:staticcheck //lint:ignore G602
 
 	case "DELETE from search.resources WHERE uid IN ($1)":
 		q, p, er = dialect.From(resources).
@@ -57,9 +58,9 @@ func useGoqu(query string, params []interface{}) (q string, p []interface{}, er 
 	case "DELETE from search.edges WHERE sourceid=$1 AND destid=$2 AND edgetype=$3":
 		q, p, er = dialect.From(edges).Prepared(true).
 			Delete().Where(
-			goqu.C("sourceid").Eq(params[0]),
-			goqu.C("destid").Eq(params[1]),
-			goqu.C("edgetype").Eq(params[2])).ToSQL()
+			goqu.C("sourceid").Eq(params[0]),         // nolint:staticcheck //lint:ignore G602
+			goqu.C("destid").Eq(params[1]),           // nolint:staticcheck //lint:ignore G602
+			goqu.C("edgetype").Eq(params[2])).ToSQL() // nolint:staticcheck //lint:ignore G602
 
 	default:
 		er = fmt.Errorf("Unable to build goqu query for [%s]", query)
